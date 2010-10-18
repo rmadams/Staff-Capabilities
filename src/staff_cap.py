f66cf37c3cd5e306fb9edaf6074ab77136988124
R Mark Adams <rmadams@epotential.com>
2010-10-18 10:11:50 -0400

    Added the stopwords reader

--- src/staff_cap.py
+++ src/staff_cap.py
@@ -7,19 +7,27 @@ Created on Oct 13, 2010
 import csv
 from wxPython.wx import *
 
-
+stopwords_file = 'C:\\Documents and Settings\\516026\\Desktop\\stopwords.txt'
 
 if __name__ == '__main__':
+    stopwords = []
+    for item in open(stopwords_file).readlines():
+        stopwords.append(item.rstrip())
+    print stopwords
     application = wxPySimpleApp()
     # Create an open file dialog
     dialog = wxFileDialog ( None, style = wxOPEN )
 
     # Show the dialog and get user input
     if dialog.ShowModal() == wxID_OK:
-        print 'Selected:', dialog.GetPath()
-
+        csv_file = dialog.GetPath()
+        print 'Selected:' + csv_file
         # The user did not select anything
     else:
         print 'Nothing was selected.'
     # Destroy the dialog
     dialog.Destroy()
+    my_reader = csv.reader(open(csv_file))
+    for item in my_reader:
+        print item
+        
