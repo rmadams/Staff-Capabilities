960869890ccb2a45f4a421cd82aa901ecae5ab69
R Mark Adams <rmadams@epotential.com>
2010-10-19 15:01:38 -0400

    Working system, includes the generation of the list of important words

--- src/staff_cap.py
+++ src/staff_cap.py
@@ -5,9 +5,11 @@ Created on Oct 13, 2010
 '''
 
 import csv
+import string
 from wxPython.wx import *
 
 stopwords_file = 'C:\\Documents and Settings\\516026\\Desktop\\stopwords.txt'
+outfile = 'C:\\Documents and Settings\\516026\\Desktop\\outwords.txt'
 
 if __name__ == '__main__':
     stopwords = []
@@ -28,6 +30,23 @@ if __name__ == '__main__':
     # Destroy the dialog
     dialog.Destroy()
     my_reader = csv.reader(open(csv_file))
+    out_words = []
+    exclude = set(string.punctuation)
     for item in my_reader:
         print item
+        s = item[5] 
+        s = ''.join(ch for ch in s if ch not in exclude)
+        s = s.lower()
+        print "My String:"
+        print s
+        for words in string.split(s):
+            if stopwords.count(words) == 0:
+                print words
+                out_words.append(words)
+    my_file = open(outfile, 'w')
+    for word in out_words:
+        my_file.write(word)
+        my_file.write('\n')
+    my_file.close()
+
         
